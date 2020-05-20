1dd94fc8c13d636e55f1b4b8f1782d447afbf55d
mysterio42 <bachana3435@gmail.com>
2020-05-21 01:01:53 +0400

    plot analogies

--- utils/plot.py
+++ utils/plot.py
@@ -4,7 +4,7 @@ matplotlib.use('Agg')
 from matplotlib import pyplot as plt
 import os
 
-FIGURES_DIR = 'figure/'
+FIGURES_DIR = 'figures/'
 
 plt.rcParams['figure.figsize'] = (13.66, 6.79)
 plt.rcParams['figure.dpi'] = 100
@@ -12,12 +12,22 @@ plt.rcParams['savefig.dpi'] = 100
 
 
 def plot_costs(costs, nm):
-    if not os.path.exists('figure'):
-        os.makedirs('figure')
+    if not os.path.exists('figures'):
+        os.makedirs('figures')
 
     plt.plot(costs)
     plt.title(f'{nm} Training Losses')
     plt.xlabel('Epoch')
     plt.ylabel('Loss')
     plt.savefig(FIGURES_DIR + 'Figure_training' + '.png')
-    plt.show()
+
+
+def plot_countries(Z, words, name):
+    if not os.path.exists('figures'):
+        os.makedirs('figures')
+
+    plt.title(f'Visualize analogies with TSNE  model weight: {name}')
+    plt.scatter(Z[:, 0], Z[:, 1])
+    for i in range(len(words)):
+        plt.annotate(s=words[i], xy=(Z[i, 0], Z[i, 1]))
+    plt.savefig(FIGURES_DIR + f'Figure_visualize_{name}' + '.png')
