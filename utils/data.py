198553c2a874850035dae9963a4715954ef77ccb
mysterio42 <bachana3435@gmail.com>
2020-05-20 19:20:14 +0400

    two method service

--- utils/data.py
+++ utils/data.py
@@ -1,10 +1,15 @@
 import pandas as pd
 
+from utils.network.glove import generate_model_name
+
 PREDS_DIR = 'preds/'
 
 
-def to_csv(name, last_word, ret_neighs):
-    with open(PREDS_DIR + name + '.txt', 'w') as f:
-        f.write(pd.DataFrame([ret_neighs], index=[last_word])
-                .to_markdown()
-                )
+def to_csv(ret_neighs):
+    rets = ret_neighs['method']
+    for meth, solution in rets.items():
+        path = PREDS_DIR + solution['word'] + '-' + 'top_neighs' + '-' + meth + '-' + generate_model_name(3) + '.txt'
+        with open(path, 'w') as f:
+            f.write(pd.DataFrame([solution['neigh_dist']], index=[solution['word']])
+                    .to_markdown()
+                    )
