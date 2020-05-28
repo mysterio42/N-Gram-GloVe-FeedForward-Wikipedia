6f0793980bb0d7a47fcee970798dc4958898bd12
mysterio42 <bachana3435@gmail.com>
2020-05-28 21:00:14 +0400

    dev for pmi

--- run.py
+++ run.py
@@ -12,15 +12,18 @@ if __name__ == '__main__':
     if args.load:
 
         exp = Exps()
-        exp.load_params('weights/glove_model_50.npz', 'embeddings/w2i.json')
+        # exp.load_params('weights/glove_model_50.npz', 'embeddings/w2i.json')
+        exp.load_pmi_params('weights/pmi_ALS.npz', 'embeddings/w2i (1).json')
+
         words = ['japan', 'japanese', 'england', 'english', 'australia', 'australian', 'china', 'chinese', 'italy',
                  'italian', 'french', 'france', 'spain', 'spanish']
-        exp.visualize_countries(words)
+        exp.visualize_countries_pmi(words)
         word = 'king'
 
-        rets_analogy = exp.analogy(word, 'man', 'woman')
+        # rets_analogy = exp.analogy(word, 'man', 'woman')
+        rets_analogy = exp.analogy_pmi(word, 'man', 'woman')
 
-        rets_neighs = exp.top_neigs(word)
+        rets_neighs = exp.top_neigs_pmi(word)
 
         to_csv(rets_neighs)
 
@@ -35,5 +38,7 @@ if __name__ == '__main__':
 
         glove_model = Glove(embedding=wiki_emb, hidden_dim=100, n_gram=10)
 
-        glove_model.build()
-        glove_model.train(epochs=20)
+        # glove_model.build()
+        # glove_model.train(epochs=20)
+        # glove_model.build_train_SVD()
+        glove_model.build_train_pmi()
